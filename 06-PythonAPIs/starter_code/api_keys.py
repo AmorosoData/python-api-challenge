6afeba03d97ca1b539d8b8043f6cac548434d7c4
Nicholas Amoroso <65302276+AmorosoData@users.noreply.github.com>
2020-08-16 07:28:04 -0700

    Update api_keys.py

--- 06-PythonAPIs/starter_code/api_keys.py
+++ 06-PythonAPIs/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "7486778e4742baab4f40c7034ceadc9f"
+weather_api_key = "ADD YOUR API"
 
 # Google API Key
-g_key = "AIzaSyDsw5NxZe8_J_gs5FtQxCMG1G5-FUd3zZc"
+g_key = "ADD YOUR API"
