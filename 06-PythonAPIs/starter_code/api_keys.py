3b068f8ef6d179787ca91d2aeb7a674dec622289
AmorosoData <amoroson@gmail.com>
2020-08-15 21:53:53 -0700

    adding finished weatherpy homowork

--- 06-PythonAPIs/starter_code/api_keys.py
+++ 06-PythonAPIs/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "7486778e4742baab4f40c7034ceadc9f"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDsw5NxZe8_J_gs5FtQxCMG1G5-FUd3zZc"
